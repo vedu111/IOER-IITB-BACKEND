b3470232f8c4546eef3bfba1344943c29a80f78a
iamrohandhiman <iamrohandhiman@gmail.com>
2025-03-08 18:06:29 +0530

    patch

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ dotenv.config();
 const app  = express()
 
 app.use(cors({
-  origin:['http://localhost:3000']
+  origin:['http://localhost:3000'],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], 
+  allowedHeaders: ["Content-Type", "Authorization"], 
+  credentials: true,  
 }))
 
 //connect db
